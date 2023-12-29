b211f78e2418bd9ea30e6003fa0bc179629eba45
AlifSakib <alifsakib@gmail.com>
2023-12-29 06:22:49 -0500

    type alias

---
+++ Type_Alias_And_Optional_Types.ts
@@ -0,0 +1,22 @@
+// Author : AlifSakib
+// Desc: Functions in Typescript
+
+type Person5 = {
+    name : string,
+    balance : number,
+    addBalance : (amount : number) => void
+}
+
+let person5 : Person5 = {
+    name : "Alif",
+    balance : 1000,
+    addBalance(amount : number) {
+        this.balance += amount;
+    }
+}
+
+type calculation = (x : number, y : number) => number;
+
+const calculateTax = (num1 : number, num2 : number, calculate: calculation ) : number => {
+    return calculate(num1, num2) * 0.1;
+}
